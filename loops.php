27de980693ca0450bc9ed58c6cc6c75cf3943774
DangleCTC <57147807+DangleCTC@users.noreply.github.com>
2019-11-25 13:02:53 -0500

    loops

--- loops.php
+++ loops.php
@@ -8,18 +8,54 @@
         - Foreach
     */
 
+    
     # For Loop - if you know the number of times its
     # going to be executed
     # @params - initializer, condition, increment
-
-    for ($i = 0; i > 10; i++){
-        echo $i;
-
+    echo 'FOR LOOPS:<br>';
+    for($i = 5; $i <= 10; $i++){
+        echo 'Number ' .$i;
+        echo '<br>';
     }
 
+    
+    # While Loop usually used over for loops if you dont know the set number/condition
+    # @params - condition
+    echo 'WHILE LOOPS:<br>';
+    $i = 0;
+    while($i < 10){
+        echo $i;
+        echo '<br>';
+        $i++;
+    }
 
+    
+    # Do While rarely used (always gonna run at least once no matter what)
+    # @params - condition
+    echo 'DO WHILE LOOPS:<br>';
+    $i = 0;
 
+    do {
+        echo $i;
+        echo '<br>';
+        $i++;
+    }
+    while($i < 10);
 
+    
+    # FOREACH loop, meant to work with array
+    echo 'FOREACH LOOPS (index):<br>';
+    $people = array('Brad', 'Jose', 'Will');
 
+    foreach ($people as $person){
+        echo $person;
+        echo '<br>';
+    }
 
+    $people = array('Brad', 'Jose', 'Will');
+
+    foreach ($people as $person){
+        echo $person;
+        echo '<br>';
+    }
 ?>
